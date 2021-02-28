c737960acccb951d3d5ee9977bc10daf012a5cb6
Lakoju <sm23770@cardiffmet.ac.uk>
2021-02-28 01:59:01 +0000

    Python package for NLP

---
+++ README.md
@@ -0,0 +1,7 @@
+# Preprocessing Text Python Package
+
+# Author: Dr Mike Lakoju
+# Date: 26 February 2021
+# Email: lakojum@yahoo.com
+
+This Python package was created by Dr Mike Lakoju for Natural Language Processing
---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open('README.md', 'r') as file:
+    long_description = file.read()
+
+setuptools.setup(
+    name = 'preprocess_mikelakoju', #Remember this name must be unique globally
+    version = '0.0.1',
+    author_email = 'lakojum@yahoo.com',
+    description = 'This is a preprocessing package for NLP',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    packages = setuptools.find_packages(),
+    classifiers = [
+        'Programming Language :: Python :: 3',
+        'License :: OSI Aproved :: MIT License',
+        'Operating System :: OS :: Independent'
+    ],
+    python_requires = '>=3.5'
+)
